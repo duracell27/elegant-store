3d30f770214d86d03d60e02c00300d9705426446
Duracell27 <volodymyrshmidt@gmail.com>
2024-03-11 22:42:02 +0200

    slider lose I win

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,19 @@
-import mobileNav from './modules/mobile-nav.js';
-import tagline from './modules/tagline.js';
-mobileNav();
+import Swiper from "swiper/bundle";
+// import "swiper/css/bundle";
+
+// import mobileNav from './modules/mobile-nav.js';
+import tagline from "./modules/tagline.js";
+
+const swiper = new Swiper(".swiper", {
+  slidesPerView: "auto",
+  spaceBetween: 24,
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+swiper();
+// mobileNav();
 tagline();
-
-
